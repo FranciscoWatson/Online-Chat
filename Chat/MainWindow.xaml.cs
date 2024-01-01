16c7118e03d98e4cfb25bc2e6527e61861dfe084
Francisco Watson <francisco@watson.com.ar>
2024-01-01 01:29:53 -0300

    Fix Grid del MainView Agrego dos columnas una para el chat y otra para los contactos que apareceran en un futuro

--- Chat/MainWindow.xaml.cs
+++ Chat/MainWindow.xaml.cs
@@ -20,37 +20,32 @@ namespace Chat
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<Message> messeges = new List<Message>();
+       
   
         public MainWindow()
         {
             InitializeComponent();
-            messeges.Add(new Message("Hola", false));
-            messeges.Add(new Message("Como estas?", false));
-            messeges.Add(new Message("Bien", true));
-            messeges.Add(new Message("Yo tambien", false));
-            MessagesControl.ItemsSource = messeges;
- 
+            ChatsDisponibles chatsDisponibles = new ChatsDisponibles();
+            ChatFrame.NavigationService.Navigate(chatsDisponibles);
+
         }
         
         
         //boton de enviar mensaje
-        private void SendMessage_Click(object sender, RoutedEventArgs e)
+        private void Chatear_Click(object sender, RoutedEventArgs e)
         {
-            // Get message written in the TextBox
-            string message = MessageTextBox.Text;
+            SelectedChat newChatPage = new SelectedChat(); // Crea una nueva instancia de la Page del chat
+                                                           // Agrega la Page del chat al contenedor correspondiente en tu ventana principal
+                                                           // (depende de cómo estés estructurando tu interfaz)
+            ChatFrame.NavigationService.Navigate(newChatPage);
 
-            messeges.Add(new Message(message, true));
-
-            // Update the ItemsSource to refresh the display
-            MessagesControl.ItemsSource = null; // Clear the ItemsSource
-            MessagesControl.ItemsSource = messeges; // Set the updated list as ItemsSource
-            // Limpiar el TextBox después de enviar el mensaje
-            MessageTextBox.Text = string.Empty;
-            
+        }
+        private void CloseChatButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChatsDisponibles chatsDisponibles = new ChatsDisponibles();
+            ChatFrame.NavigationService.Navigate(chatsDisponibles);
         }
 
-            
-        
+
     }
 }
---
+++ Chat/SelectedChat.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chat
+{
+    /// <summary>
+    /// Interaction logic for SelectedChat.xaml
+    /// </summary>
+    public partial class SelectedChat : Page
+    {
+        List<Message> messeges = new List<Message>();
+        public SelectedChat()
+        {
+            InitializeComponent();
+            messeges.Add(new Message("Hola", false));
+            messeges.Add(new Message("Como estas?", false));
+            messeges.Add(new Message("Bien", true));
+            messeges.Add(new Message("Yo tambien", false));
+            MessagesControl.ItemsSource = messeges;
+        }
+
+        private void SendMessage_Click(object sender, RoutedEventArgs e)
+        {
+            // Get message written in the TextBox
+            string message = MessageTextBox.Text;
+
+            messeges.Add(new Message(message, true));
+
+            // Update the ItemsSource to refresh the display
+            MessagesControl.ItemsSource = null; // Clear the ItemsSource
+            MessagesControl.ItemsSource = messeges; // Set the updated list as ItemsSource
+            // Limpiar el TextBox después de enviar el mensaje
+            MessageTextBox.Text = string.Empty;
+        }
+        
+
+    }
+}
